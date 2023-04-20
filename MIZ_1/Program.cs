35399d3027e6b4c6b329a739937cefdcc1f80dcc
AlexProstolucky <alexprostolucky@gmail.com>
2023-04-20 20:15:26 +0300

    Добавьте файлы проекта.

---
+++ MIZ_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n;
+        Console.WriteLine("Введіть кількість букв:");
+        n = Int32.Parse(Console.ReadLine());
+        string[] str = { "відбудова", "стратегія", "місія", "реформа", "візія", "модернізація", "екосистема", "реконструкція", "політика", "спроможність" };  
+        var zap = from st in str
+            where st.Length == n
+                select st;
+
+        Console.WriteLine($"Результат пошуку слів з кількістю букв ({n}):");
+        foreach (var st in zap) 
+        {
+            Console.Write($"{st} ");
+        }
+    }
+}
